5389e9da80abaa1eea715d9ed77f5de4a2d8b25a
PrantikaDey16 <136480640+PrantikaDey16@users.noreply.github.com>
2025-07-27 12:16:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Hello Everyone this is our first application!!")
+st.write("This is SCIT in Pune")
+
+df=pd.DataFrame({
+    'first column' : [1,2,3,4]
+    'second column' : [10,20,30,40]
+})
+
+st.write("Here is the dataframe")
+st.write(df)
+
+chart_data=pd.DataFrame(np.random.randn(20,3),columns=['a','b','c'])
+st.bar_chart(chart_data)
